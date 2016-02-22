37525956e032472820e08f64096e9ed682309d4b
Fernando Costa <fernandocosta@mail.asconsultoriaeprojetos.com.br>
2016-02-22 17:48:42 -0400

    Added basic list with fixed parameter and beers detailed view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,82 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+    .controller('AppCtrl', function ($scope, $ionicModal) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/search.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+        // Open the search modal
+        $scope.openModal = function () {
+            $scope.modal.show();
+        };
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        // Triggered in the login modal to close it
+        $scope.closeModal = function () {
+            $scope.modal.hide();
+        };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+    })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    .controller('BeersListCtrl', function ($scope, $stateParams, $http) {
+        //That's mine! =D Change it later
+        var apiKey = '53bdaa14fe3d0a7db5fe60cb7c4facb9';
+        var url = 'https://salty-taiga-88147.herokuapp.com/beers';
+        var options = {
+            key: apiKey,
+            abv: '10'
+        };
+        $http({
+            method: 'GET',
+            url: url,
+            params: options
+        })
+            .then(
+            function (response) {
+                console.info('Success');
+                console.log(response);
+                $scope.beers = response.data.data;
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+            },
+            function (response) {
+                console.info('Error');
+                console.log(response);
+            });
+
+    })
+    .controller('BeerDescriptionCtrl', function ($scope, $stateParams, $http) {
+        var url = 'https://salty-taiga-88147.herokuapp.com/beer/';
+        if(angular.isDefined($stateParams.idBeer)){
+            url += $stateParams.idBeer;
+        }
+        var apiKey = '53bdaa14fe3d0a7db5fe60cb7c4facb9';
+
+        var options = {
+            key: apiKey
+        };
+        var request = {
+            method: 'GET',
+            url: url,
+            params: options
+        };
+
+        $http(request)
+            .then(
+            function (response) {
+                console.info('Success');
+                console.log(response);
+                $scope.beer = response.data.data;
+
+            },
+            function (response) {
+                console.info('Error');
+                console.log(response);
+            });
+    });
